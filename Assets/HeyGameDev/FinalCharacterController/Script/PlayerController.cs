b176df309dd7f63410b67041c4cc5024d66e2c99
Deepak Lingala <142999623+DeepakLingala@users.noreply.github.com>
2026-05-02 13:22:52 +0530

    PART - 5 COMPLETED
    In Part - 5 , i do the player movement and response by giving the inputs in the keyboard. i do the follows the physics and vectors in the scripts.

--- Assets/HeyGameDev/FinalCharacterController/Script/PlayerController.cs
+++ Assets/HeyGameDev/FinalCharacterController/Script/PlayerController.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace HeyGameDev.FinalCharacterController {
+
+    public class PlayerController : MonoBehaviour
     {
-        
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Camera _playerCamera;
+
+        public float runAcceleration = 0.25f;
+        public float runSpeed = 4f;
+        public float drag = 0.1f;
+
+        private PlayerLocomotionInput _playerLocomotionInput;
+
+        private void Awake()
+        {
+            _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        }
+
+        private void Update()
+        {
+            Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
+            Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
+            Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
+
+            Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
+            Vector3 newVelocity = _characterController.velocity + movementDelta;
+
+            //Add the drag to Player
+            Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
+            newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
+            newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
+
+            // Move character (Unity suggests only calling this once per trick).
+            _characterController.Move(newVelocity * Time.deltaTime);
+        }
     }
 }
