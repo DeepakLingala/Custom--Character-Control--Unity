02d65b16b2f0c92209893c19a2c29c2337f6ba97
Deepak Lingala <142999623+DeepakLingala@users.noreply.github.com>
2026-05-25 15:46:36 +0530

    setup 12.1
    done

--- Assets/HeyGameDev/FinalCharacterController/Script/PlayerController.cs
+++ Assets/HeyGameDev/FinalCharacterController/Script/PlayerController.cs
@@ -276,7 +276,6 @@ namespace HeyGameDev.FinalCharacterController
         {
             Vector3 normal = CharacterControllerUtils.GetNormalWithSphereCast(_characterController, _groundLayers);
             float angle = Vector3.Angle(normal, Vector3.up);
-            print(angle);
             bool validAngle = angle <= _characterController.slopeLimit;
 
             return _characterController.isGrounded && validAngle;
